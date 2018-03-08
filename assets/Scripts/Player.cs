1755c4cca81d36a9a891f5073a637904462ffe84
NickNil <Nicklas.l.nilsen@gmail.com>
2018-03-08 14:04:58 +0100

    first commit

---
+++ assets/Scripts/Player.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+[RequireComponent(typeof(PlayerAnimationController))]
+
+public class Player : MonoBehaviour {
+    Controller2D controller;
+    PlayerAnimationController animationController;
+
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    public float accelerationTimeAirBorne = .2f;
+    public float accelerationTimeGrounded = .1f;
+    public float moveSpeed = 12;
+    public float backJumpDistance = 4f;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timetoWallUnstick;
+
+    Vector2 input;
+    bool wallSliding;
+    int wallDirX = 0;
+
+    float gravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+    Vector3 velocity;
+    float velocityXsmoothing;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<Controller2D>();
+        animationController = GetComponent<PlayerAnimationController>();
+        animationController.stateInfo.Reset();
+        animationController.stateInfo.faceRight = true;
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        print("Gravity: " + gravity + " jump velocity: " + maxJumpVelocity);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+
+        if (!controller.edgeDet.edge)
+        {
+            float targetVelocityX = input.x * moveSpeed;
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXsmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirBorne);
+        }
+        
+
+        HandleWallSliding();
+        
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        HandleInput();
+
+        if (!controller.edgeDet.edge)
+            velocity.y += gravity * Time.deltaTime;
+
+
+        animationController.stateInfo.velocity = velocity;
+        animationController.stateInfo.input = input;
+        if (controller.collisions.below)
+        {
+            animationController.stateInfo.grounded = true;
+        }
+
+        if (animationController.stateInfo.grounded || animationController.inBackJump)
+            animationController.Flip(input.x);
+        else
+            animationController.Flip(velocity.x);
+        //Debug.Log("velocity.x after damp: " + velocity.x);
+        
+       
+        animationController.ContinueState();
+
+        controller.Move(velocity * Time.deltaTime, input);
+
+        animationController.stateInfo.Reset();
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animationController.stateInfo.crouch = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            animationController.stateInfo.crouch = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animationController.stateInfo.attack = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animationController.stateInfo.jump = true;
+            if (wallSliding)
+            {
+                if (wallDirX == (int)input.x)
+                {
+                    Debug.Log("test jump climb");
+                    velocity.x = -wallDirX * wallJumpClimb.x;
+                    velocity.y = wallJumpClimb.y;
+                }
+                else if (input.x == 0)
+                {
+                    Debug.Log("test jump off");
+
+                    velocity.x = -wallDirX * wallJumpOff.x;
+                    velocity.y = wallJumpOff.y;
+                }
+                else
+                {
+                    Debug.Log("test jump leap");
+                    velocity.x = -wallDirX * wallLeap.x;
+                    velocity.y = wallLeap.y;
+                }
+            }
+            if (controller.collisions.below)
+            {
+                velocity.y = maxJumpVelocity;
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+
+            if (velocity.y > minJumpVelocity)
+            {
+                velocity.y = minJumpVelocity;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animationController.stateInfo.backJump = true;
+            animationController.inBackJump = true;
+            if (animationController.stateInfo.faceRight)
+            {
+                velocity.x = backJumpDistance;
+            }
+            else
+            {
+                velocity.x = -backJumpDistance;
+            }
+        }
+    }
+
+    void HandleWallSliding()
+    {
+        wallSliding = false;
+
+        if (controller.edgeDet.edge)//edge detection
+        {
+            velocity.y = 0;
+        }
+
+        else if ((controller.collisions.left || controller.collisions.right) && (!controller.collisions.below && velocity.y < 0))
+        {
+            wallSliding = true;
+            animationController.stateInfo.wallSliding = true;
+
+            if (velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+            if (timetoWallUnstick > 0)
+            {
+                velocityXsmoothing = 0;
+                if (input.x != wallDirX && input.x != 0)
+                {
+                    timetoWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timetoWallUnstick = wallStickTime;
+                }
+            }
+            else
+            {
+                timetoWallUnstick = wallStickTime;
+            }
+        }
+    }
+}
---
+++ assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,329 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//extend player, get player state info
+public class PlayerAnimationController : MonoBehaviour {
+
+    public StateInfo stateInfo;
+
+    const string kIdleAnim = "Idle";
+    const string kRunAnim = "Run";
+    const string kJumpAnim = "Jump";
+    const string kFallAnim = "Fall";
+    const string kLandAnim = "Land";
+    const string kJumpTransAnim = "Jump_Transition";
+    const string kCrouchAnim = "Crouch";
+    const string kCrouchIdleAnim = "Crouch Idle";
+    const string kCrawlAnim = "Crawl";
+    const string kAttack1Anim = "Attack1";
+    const string kAttack1FinAnim = "Attack1 Finish";
+    const string kAttack2Anim = "Attack2";
+    const string kAttack2FinAnim = "Attack2 Finish";
+    const string kAttack3Anim = "Attack3";
+    const string kAttack3FinAnim = "Attack3 Finish";
+    const string kWallSlideAnim = "Wall Cling";
+    const string kWallJumpAnim = "Wall Jump";
+    const string kBackJumpAnim = "Back Jump";
+
+    enum State
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling,
+        Landing,
+        Jump_Transitioning,
+        Crouching,
+        Crouch_Idle,
+        Crawling,
+        Attack1_Finishing,
+        Attacking1,
+        Attack2_Finishing,
+        Attacking2,
+        Attack3_Finishing,
+        Attacking3,
+        WallSliding,
+        WallJumping,
+        BackJumping
+    }
+
+    State state;
+    private Animator animator;
+    private bool inAttack = false;
+    [HideInInspector]
+    public bool inBackJump = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        stateInfo.count = 0;
+    }
+
+   public void Flip(float horizontal)
+    {
+        if (inBackJump)
+        {
+            return;
+        }
+        else
+        {
+            if (horizontal > 0 && stateInfo.faceRight || horizontal < 0 && !stateInfo.faceRight)
+            {
+                if (stateInfo.faceRight)
+                {
+                    stateInfo.faceRight = false;
+                }
+                else
+                {
+                    stateInfo.faceRight = true;
+                }
+                Vector3 theScale = transform.localScale;
+
+                theScale.x *= -1;
+
+                transform.localScale = theScale;
+            }
+        }
+    }
+
+    private void EnterState(State state)
+    {
+        switch (state)
+        {
+            case State.Idle:
+                animator.Play(kIdleAnim);
+                break;
+            case State.Running:
+                animator.Play(kRunAnim);
+                break;
+            case State.Jumping:
+                animator.Play(kJumpAnim);
+                break;
+            case State.Falling:
+                animator.Play(kFallAnim);
+                break;
+            case State.Landing:
+                animator.Play(kLandAnim);
+                break;
+            case State.Jump_Transitioning:
+                animator.Play(kJumpTransAnim);
+                break;
+            case State.Crouching:
+                animator.Play(kCrouchAnim);
+                break;
+            case State.Crouch_Idle:
+                animator.Play(kCrouchIdleAnim);
+                break;
+            case State.Crawling:
+                animator.Play(kCrawlAnim);
+                break;
+            case State.Attacking1:
+                animator.Play(kAttack1Anim);
+                break;
+            case State.Attack1_Finishing:
+                animator.Play(kAttack1FinAnim);
+                break;
+            case State.Attacking2:
+                animator.Play(kAttack2Anim);
+                break;
+            case State.Attack2_Finishing:
+                animator.Play(kAttack2FinAnim);
+                break;
+            case State.Attacking3:
+                animator.Play(kAttack3Anim);
+                break;
+            case State.Attack3_Finishing:
+                animator.Play(kAttack3FinAnim);
+                break;
+            case State.WallSliding:
+                animator.Play(kWallSlideAnim);
+                break;
+            case State.WallJumping:
+                animator.Play(kWallJumpAnim);
+                break;
+            case State.BackJumping:
+                animator.Play(kBackJumpAnim);
+                break;
+
+        }
+
+        this.state = state;
+    }
+
+    public void ContinueState()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                inBackJump = false;
+                if (stateInfo.attack)
+                    EnterState(State.Attacking1);
+                else if (stateInfo.backJump)
+                    EnterState(State.BackJumping);
+                else if (stateInfo.crouch)
+                    EnterState(State.Crouching);
+                else if (stateInfo.jump)
+                    EnterState(State.Jumping);
+                else if (stateInfo.input.x != 0)
+                    EnterState(State.Running);
+                break;
+            case State.Running:
+                Debug.Log("velocity.x: " + stateInfo.input.x);
+                if (stateInfo.jump)
+                    EnterState(State.Jumping);
+                else if (stateInfo.input.x == 0)
+                    EnterState(State.Idle);
+                else if (stateInfo.backJump)
+                    EnterState(State.BackJumping);
+                break;
+            case State.Jumping:
+                if (stateInfo.wallSliding)
+                    EnterState(State.WallSliding);
+                else if (stateInfo.velocity.y <= 0)
+                    EnterState(State.Jump_Transitioning);
+                break;
+            case State.Jump_Transitioning:
+                if (stateInfo.wallSliding)
+                    EnterState(State.WallSliding);
+                else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                        EnterState(State.Falling);
+                break;
+            case State.Falling:
+                if (stateInfo.wallSliding)
+                    EnterState(State.WallSliding);
+                else if (stateInfo.grounded)
+                    EnterState(State.Landing);
+                break;
+            case State.Landing:
+                if (stateInfo.crouch)
+                    EnterState(State.Crouching);
+                else if (stateInfo.jump)
+                    EnterState(State.Jumping);
+                else if (stateInfo.input.x != 0)
+                    EnterState(State.Running);
+                else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                    EnterState(State.Idle);
+                break;
+            case State.Crouching:
+                if (!stateInfo.crouch)
+                    EnterState(State.Idle);
+                else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                    EnterState(State.Crouch_Idle);
+                break;
+            case State.Crouch_Idle:
+                if (!stateInfo.crouch)
+                    EnterState(State.Idle);
+                else if (stateInfo.input.x != 0)
+                    EnterState(State.Crawling);
+                break;
+            case State.Crawling:
+                if (!stateInfo.crouch)
+                    EnterState(State.Idle);
+                else if (stateInfo.input.x == 0)
+                    EnterState(State.Crouch_Idle);
+                break;
+            case State.Attacking1:
+                if (stateInfo.attack)
+                {
+                    inAttack = true;
+                }
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    if (inAttack)
+                    {
+                        EnterState(State.Attacking2);
+                        inAttack = false;
+                    }
+                    else
+                        EnterState(State.Attack1_Finishing);
+                }
+                break;
+            case State.Attack1_Finishing:
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                    EnterState(State.Idle);
+                break;
+            case State.Attacking2:
+                if (stateInfo.attack)
+                {
+                    inAttack = true;
+                }
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    if (inAttack)
+                    {
+                        EnterState(State.Attacking3);
+                        inAttack = false;
+                    }
+                    else
+                        EnterState(State.Attack2_Finishing);
+                }
+                break;
+            case State.Attack2_Finishing:
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                    EnterState(State.Idle);
+                break;
+            case State.Attacking3:
+                if (stateInfo.attack)
+                {
+                    inAttack = true;
+                }
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    if (inAttack)
+                    {
+                        EnterState(State.Attacking1);
+                        inAttack = false;
+                    }
+                    else
+                        EnterState(State.Attack3_Finishing);
+                }
+                break;
+            case State.Attack3_Finishing:
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                    EnterState(State.Idle);
+                break;
+            case State.WallSliding:
+                if (stateInfo.jump)
+                    EnterState(State.WallJumping);
+                else if (stateInfo.grounded)
+                    EnterState(State.Idle);
+                break;
+            case State.WallJumping:
+                if (stateInfo.wallSliding)
+                    EnterState(State.WallSliding);
+                else if (stateInfo.velocity.y <= 0)
+                    EnterState(State.Jump_Transitioning);
+                break;
+            case State.BackJumping:
+                
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    EnterState(State.Idle);
+                    
+                }
+                break;
+
+        }
+    }
+
+    public struct StateInfo
+    {
+        public Vector2 input;
+        public Vector3 velocity;
+        public bool faceRight;
+        public bool jump, grounded, crouch, attack;
+        public bool wallSliding, backJump;
+        public float count;
+        
+
+        public void Reset()
+        {
+            jump = false;
+            grounded = false;
+            attack = false;
+            wallSliding = false;
+            backJump = false;
+        }
+    }
+}
